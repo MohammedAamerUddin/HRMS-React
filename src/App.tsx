5de7d01afa14deb70bca61faca13c742caa511ae
Mohammed Aameruddin <mdaameruddin01@gmail.com>
2026-03-03 03:00:06 +0530

    Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='nav'>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className='container flex'>
+        <div className='card flex'>
+          <h2 style={{ color: "rgb(8, 44, 253)" }}>Admin</h2>
+        </div>
+        <div className='card flex'>
+          <h2 style={{ color: "rgb(30, 255, 0)" }}>Employee</h2>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
