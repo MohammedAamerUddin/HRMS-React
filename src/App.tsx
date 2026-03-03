77e4d908c175e8fe01220dcd0d7d28f069cfb4d2
Mohammed Aameruddin <mdaameruddin01@gmail.com>
2026-03-03 19:40:16 +0530

    Created endpoints for admin and employee

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
-import './App.css'
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Admin from "./pages/admin";
+import "./App.css";
+import Employee from "./pages/employee";
 
-function App() {
+function Home() {
+  const navigate = useNavigate();
 
   return (
     <>
-      <div className='nav'>
-      </div>
+      <div className="nav"></div>
 
-      <div className='container flex'>
-        <div className='card flex'>
+      <div className="container flex">
+        <div
+          className="card flex"
+          onClick={() => navigate("/admin")}
+        >
           <h2 style={{ color: "rgb(8, 44, 253)" }}>Admin</h2>
         </div>
-        <div className='card flex'>
+
+        <div className="card flex" onClick={() => navigate("/employee")}>
           <h2 style={{ color: "rgb(30, 255, 0)" }}>Employee</h2>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/employee" element={<Employee />} />
+    </Routes>
+  );
+}
+
+export default App;
